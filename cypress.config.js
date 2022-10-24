45975164e2ffa19835b41c6069f7e74968ec33cd
Christian Leon <jesusleonangulo@hotmail.com>
2022-10-24 15:31:08 -0500

    Pruebas Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env:{
-    base_url: 'http://localhost:49724'
+    base_url: 'http://localhost:50063'
   },
   e2e: {
+    experimentalSessionAndOrigin:true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
