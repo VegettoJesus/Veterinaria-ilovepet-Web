610c29215a43304858c596086dfc9eb6d34c6eef
Christian Leon <jesusleonangulo@hotmail.com>
2022-11-03 12:33:59 -0500

    Actualizacion del Proyecto

--- proyecto_veterinaria/src/main/java/com/veterinaria/proyecto_veterinaria/Entidades/Historial_MedicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.veterinaria.proyecto_veterinaria.Entidades;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface Historial_MedicoRepository extends PagingAndSortingRepository<Historial_Medico,Long>{
-    
-}
